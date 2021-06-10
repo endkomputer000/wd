be8e3b2c5e89a8039fdb2f3c5360fd2e21aafd74
endkomputer000 <end.komputer@gmail.com>
2021-06-10 19:45:35 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@
  * Link
  */
 export const GOOGLE_CALENDAR_LINK = `https://calendar.google.com/calendar/event?action=TEMPLATE&dates=20201003T040000Z%2F20201003T060000Z&text=Dinda+%26+Indra+Wedding&details=Dinda+%26+Indra+Wedding`;
-export const GOOGLE_MAPS_LINK = `https://g.page/BurzaHotelYogyakarta?share`;
+export const GOOGLE_MAPS_LINK = `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.6788002842386!2d110.36444191533216!3d-7.823780879874278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a57bb035eaa4d%3A0x3ab5982f33aae7af!2sBurza%20Hotel%20Yogyakarta!5e0!3m2!1sen!2sid!4v1623329117374!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`;
 
 /**
  * Wedding time
