ece764c88c0b5ef5477b9fd53be69605ddd269c2
endkomputer000 <end.komputer@gmail.com>
2021-06-10 20:02:56 +0700

    Update index.js

--- src/components/LocationSection/index.js
+++ src/components/LocationSection/index.js
@@ -18,7 +18,7 @@ function LocationSection() {
                   rel="noreferrer"
                   style={{ color: '#828282' }}
                 >
-                  <strong>HARRIS Hotel Sentraland Semarang</strong>
+                  <strong>Burza Hotel Yogyakarta</strong>
                 </a>{' '}
                 <br />
                 Jl. Ki Mangunsarkoro No.36, Karangkidul, Kec. Semarang Tengah, <br />
@@ -29,7 +29,7 @@ function LocationSection() {
           <div className="row">
             <div className="col-md-10 col-md-offset-1">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7920.293266961091!2d110.4250066322774!3d-6.992004594949059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e708ca11d0e6f61%3A0x8c359fe27c58195d!2sHARRIS%20Hotel%20Sentraland%20Semarang!5e0!3m2!1sen!2sid!4v1590977401700!5m2!1sen!2sid"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3952.6788002842386!2d110.36444191533216!3d-7.823780879874278!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a57bb035eaa4d%3A0x3ab5982f33aae7af!2sBurza%20Hotel%20Yogyakarta!5e0!3m2!1sen!2sid!4v1623329117374!5m2!1sen!2sid"
                 width="100%"
                 height="450"
                 frameBorder="0"
